be11ec2e7695a7a71e7fc4eebb6eaa3be88029c7
mhugunin <mhugunin@andrew.cmu.edu>
2021-05-04 12:11:21 -0400

    multigoal but it does get slow and end up trapped in pockets also the estimate update upon finding a goal is a bit jank

--- runtest.m
+++ runtest.m
@@ -58,111 +58,149 @@ caught = 0;
 
 display_n = 3; %render every nth iteration
 
-for i = 1:1000
-    %draw the positions
-    if (hr ~= -1)
-        delete(hr);
-    end
-    
-    [~, n] = max(goalmap(:));
-    [x_max, y_max] = ind2sub(size(goalmap), n);
-        
-    if(mod(i, display_n)==0)
-        figure(1);
-        axes(sp1);
-        hr = text(robotpos(1), robotpos(2), 'R', 'Color', 'w', 'FontWeight', 'bold');
-        hr0 = scatter(robotpos(1), robotpos(2), 10, 'w', 'filled');
+sourcecount = 3;
+sourcelog = zeros(sourcecount, 2);
+sourcerad = 40;
+s = 1;
 
-        axes(sp2);
-        imshow((exploredmap.*~obsmap + 0.5*(~exploredmap))');
-
-        axes(sp3);
-        imshow(goalmap/max(max(goalmap)));
-        hold on;
-        
-        if (goal_guess_plot2 ~= -1) 
-            delete(goal_guess_plot2);
+while s <= sourcecount
+    for i = 1:1000
+        %draw the positions
+        if (hr ~= -1)
+            delete(hr);
         end
-        goal_guess_plot2 = scatter(y_max, x_max, 'm', 'LineWidth', 2);
-        hold off;
 
-        axes(sp1);
-        if (goal_guess_plot ~= -1)
-            delete(goal_guess_plot);
+        [maxval, n] = max(goalmap(:));
+        [x_max, y_max] = ind2sub(size(goalmap), n)
+%         ids = find(goalmap(:)==maxval);
+%         [x_max_arr, y_max_arr] = ind2sub(size(goalmap), ids);
+%         x_max = sum(x_max_arr)/length(x_max_arr);
+%         y_max = sum(y_max_arr)/length(y_max_arr);
+
+        if(mod(i, display_n)==0)
+            figure(1);
+            axes(sp1);
+            hr = text(robotpos(1), robotpos(2), 'R', 'Color', 'w', 'FontWeight', 'bold');
+            hr0 = scatter(robotpos(1), robotpos(2), 10, 'w', 'filled');
+
+            axes(sp2);
+            imshow((exploredmap.*~obsmap + 0.5*(~exploredmap))');
+
+            axes(sp3);
+            imshow(goalmap/max(max(goalmap)));
+            hold on;
+
+            if (goal_guess_plot2 ~= -1) 
+                delete(goal_guess_plot2);
+            end
+            goal_guess_plot2 = scatter(y_max, x_max, 'm', 'LineWidth', 2);
+            hold off;
+
+            axes(sp1);
+            if (goal_guess_plot ~= -1)
+                delete(goal_guess_plot);
+            end
+            goal_guess_plot = scatter(y_max, x_max, 'm', 'LineWidth', 2); 
         end
-        goal_guess_plot = scatter(y_max, x_max, 'm', 'LineWidth', 2); 
-    end
-    
-    %call robot planner to find what they want to do
-    %tStart = tic;
-    % want our planner to return entire path to next frontier location
-    localplan = robotplanner(envmap, obsmap, exploredmap, [y_max x_max], robotpos);
-%     localplan = [robotpos(1)-1 robotpos(2)-1; robotpos(1)-2, robotpos(2)-2]; %TEMP STAND-IN
-%     
-%     if i > 20
-%         localplan = [robotpos(1)-1 robotpos(2); robotpos(1)-2, robotpos(2)];
-%     end
-    
-    %compute movetime for the target
-    %tElapsed = toc(tStart);
-    %timeTaken = tElapsed*1000; % in ms
-    %movetime = max(1, ceil(timeTaken/200));
-    
-    for j = 1:size(localplan, 1)
-        newrobotpos = localplan(j, :);
-        %check that the new commanded position is valid
-        if (newrobotpos(1) < 1 || newrobotpos(1) > size(envmap, 1) || ...
-                newrobotpos(2) < 1 || newrobotpos(2) > size(envmap, 2))
-            fprintf(1, 'ERROR: out-of-map robot position commanded\n');
-            return;
-        elseif (obsmap(newrobotpos(1), newrobotpos(2)) ~= 0)
-            fprintf(1, 'ERROR: invalid robot position commanded\n');
-            return;
-        elseif (abs(newrobotpos(1)-robotpos(1)) > 1 || abs(newrobotpos(2)-robotpos(2)) > 1)
-            fprintf(1, 'ERROR: invalid robot move commanded\n');
-            return;
-        end        
 
-        %make the moves
-        robotpos = double(newrobotpos);
-        % exploredmap(robotpos(1), robotpos(2)) = 1;
-        numofmoves = numofmoves + 1;
+        %call robot planner to find what they want to do
+        %tStart = tic;
+        % want our planner to return entire path to next frontier location
+        localplan = robotplanner(envmap, obsmap, exploredmap, [y_max x_max], robotpos);
+    %     localplan = [robotpos(1)-1 robotpos(2)-1; robotpos(1)-2, robotpos(2)-2]; %TEMP STAND-IN
+    %     
+    %     if i > 20
+    %         localplan = [robotpos(1)-1 robotpos(2); robotpos(1)-2, robotpos(2)];
+    %     end
+
+        %compute movetime for the target
+        %tElapsed = toc(tStart);
+        %timeTaken = tElapsed*1000; % in ms
+        %movetime = max(1, ceil(timeTaken/200));
+
+        for j = 1:size(localplan, 1)
+            newrobotpos = localplan(j, :);
+            %check that the new commanded position is valid
+            if (newrobotpos(1) < 1 || newrobotpos(1) > size(envmap, 1) || ...
+                    newrobotpos(2) < 1 || newrobotpos(2) > size(envmap, 2))
+                fprintf(1, 'ERROR: out-of-map robot position commanded\n');
+                return;
+            elseif (obsmap(newrobotpos(1), newrobotpos(2)) ~= 0)
+                fprintf(1, 'ERROR: invalid robot position commanded\n');
+                return;
+            elseif (abs(newrobotpos(1)-robotpos(1)) > 1 || abs(newrobotpos(2)-robotpos(2)) > 1)
+                fprintf(1, 'ERROR: invalid robot move commanded\n');
+                return;
+            end        
+
+            %make the moves
+            robotpos = double(newrobotpos);
+            % exploredmap(robotpos(1), robotpos(2)) = 1;
+            numofmoves = numofmoves + 1;
+
+            %pause(0.1);
+        end
+
+        %Update contamination map
+        contam_reading = envmap(robotpos(1), robotpos(2));
         
-        %pause(0.1);
-    end
-    
-    %Update contamination map
-    contam_reading = envmap(robotpos(1), robotpos(2));
-    exp_dist = max(0, 35*((-1*log(contam_reading)).^(3/4)));
-    
-    dis = sqrt((x-robotpos(1)).^2+(y-robotpos(2)).^2);
-    c = 5+15*(1-contam_reading);
-    
-    p_gau_far = 1+(contam_reading*(exp(-1*(dis-exp_dist).^2/(2*c^2))-1));
-    p_gau_near = 1+(contam_reading*(exp(-1*(dis-exp_dist).^2/(2.5*2*c^2))-1));
-    p_gau = p_gau_far.*(dis>exp_dist)+p_gau_near.*(dis<=exp_dist);
-    if(mod(i, display_n)==0)
-        axes(sp4);
-        imshow(p_gau);
-    end
-%     pause(0.1);
-    
-    goalmap = goalmap.*p_gau;
-    goalmap = goalmap ./ sum(sum(goalmap)); %normalize probs to sum of 1
-    
-    %TODO: Implement stopping condition -> should be signaled by the
-    %planner when it thinks it found the source
-    %check if target is caught
-%     for source_ind = 1:size(sources, 1)
-%          if (abs(robotpos(1)-sources(source_ind, 1)) <= 1 && abs(robotpos(2)-sources(source_ind, 2)) <= 1)
-%              caught = 1;
-%              break; %Condition will be different if multigoal
-%          end
-%     end
-    if (abs(robotpos(1) - y_max) <= 1 && abs(robotpos(2) - x_max) <= 1)
-        caught = 1;
-        fprintf(1, 'Distance from actual goal = %f\n', sqrt((sources(1,1) - y_max)^2 + (sources(1,2) - x_max)^2));
-        break;
+        for explored_s = 1:s-1
+            old_dis = norm([robotpos(1)-sourcelog(explored_s, 2), robotpos(2)-sourcelog(explored_s, 1)]);
+            if(old_dis < sourcerad)
+                contam_reading = 0;
+                explored_s = s;
+            else
+                exp_reading = 0.5*exp(-1*(old_dis/35)^(4/3)); %inverse of sensor relation
+                contam_reading = contam_reading - exp_reading;
+            end
+        end
+        exp_dist = max(0, 35*((-1*log(contam_reading)).^(3/4)));
+        if(contam_reading == 0)
+            exp_dist = 0;
+        end
+
+        if(contam_reading > 0)
+            dis = sqrt((x-robotpos(1)).^2+(y-robotpos(2)).^2);
+            c = 5+15*(1-contam_reading);
+
+            p_gau_far = 1+(contam_reading*(exp(-1*(dis-exp_dist).^2/(2*c^2))-1));
+            p_gau_near = 1+(contam_reading*(exp(-1*(dis-exp_dist).^2/(2.5*2*c^2))-1));
+            p_gau = p_gau_far.*(dis>exp_dist)+p_gau_near.*(dis<=exp_dist);
+        else
+            p_gau = ones(size(dis));
+        end
+       
+        if(mod(i, display_n)==0)
+            axes(sp4);
+            imshow(p_gau);
+        end
+    %     pause(0.1);
+
+        goalmap = goalmap.*p_gau;
+        goalmap = goalmap ./ sum(sum(goalmap)); %normalize probs to sum of 1
+
+        %TODO: Implement stopping condition -> should be signaled by the
+        %planner when it thinks it found the source
+        %check if target is caught
+    %     for source_ind = 1:size(sources, 1)
+    %          if (abs(robotpos(1)-sources(source_ind, 1)) <= 1 && abs(robotpos(2)-sources(source_ind, 2)) <= 1)
+    %              caught = 1;
+    %              break; %Condition will be different if multigoal
+    %          end
+    %     end
+        if (abs(robotpos(1) - y_max) <= 1 && abs(robotpos(2) - x_max) <= 1)
+            caught = 1;
+            fprintf(1, 'Distance from actual goal = %f\n', sqrt((sources(1,1) - y_max)^2 + (sources(1,2) - x_max)^2));
+            sourcelog(s, :) = [x_max, y_max];
+            s = s + 1;
+            dis = sqrt((x-robotpos(1)).^2+(y-robotpos(2)).^2);
+            for (sub_s = 1:s-2)
+                dis = dis.*sqrt((x-sourcelog(sub_s,2)).^2+(y-sourcelog(sub_s,1).^2));
+            end
+            goalmap = dis;
+            goalmap = goalmap / max(max(goalmap));
+            %goalmap = ones(size(envmap)) / (size(envmap, 1) * size(envmap, 2));
+        end
     end
 end
 
