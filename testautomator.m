f4bd06871639a1495aad1792f4a086c1b3903f21
mhugunin <mhugunin@andrew.cmu.edu>
2021-05-05 13:35:01 -0400

    last commit before present

--- testautomator.m
+++ testautomator.m
@@ -1,12 +1,12 @@
 %mex COPTIMFLAGS="-O3"  planner.cpp
-map_ind = 3;
-source_ind = 2;
-start_ind = 3;
+map_ind = 1;
+source_ind = 1;
+start_ind = 1;
 
 
 maps = ["map3.png", "map4.png", "map5.png"];
 sources = {[105, 35; 50, 100], [70, 30; 60, 125]};
-starts = {[30, 20], [165, 165], [40, 10]};
+starts = {[30, 20], [165, 165], [40, 10], [120, 40]};
 
 tic();
 runtest(starts{start_ind}, maps(map_ind), sources{source_ind});
