8539b5401d73947aea607103edebce3977a8d044
mhugunin <mhugunin@andrew.cmu.edu>
2021-05-04 22:22:53 -0400

    local minor changes

--- testautomator.m
+++ testautomator.m
@@ -1,12 +1,12 @@
 %mex COPTIMFLAGS="-O3"  planner.cpp
 map_ind = 1;
 source_ind = 1;
-start_ind = 1;
+start_ind = 2;
 
 
 maps = ["map3.png", "map4.png", "map5.png"];
 sources = {[105, 35; 50, 100]};
-starts = {[30, 20]};
+starts = {[30, 20], [165, 165]};
 
 tic();
 runtest(starts{start_ind}, maps(map_ind), sources{source_ind});
